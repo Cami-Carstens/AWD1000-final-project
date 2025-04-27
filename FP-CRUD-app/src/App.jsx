4024917f76bc5ecfac14055c14490db14143ca71
Cami-Carstens <camisuec@aol.com>
2025-04-26 21:26:53 -0500

    React CRUD 4/26 commit

--- FP-CRUD-app/src/App.jsx
+++ FP-CRUD-app/src/App.jsx
@@ -2,116 +2,222 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {nanoid} from 'nanoid';
-import React, {useState} from 'react';
-import AddStudent from './addStudent';
+import React, {useState, useEffect} from 'react';
+import AddStudent from './components/AddStudent';
+import _ from 'lodash';
+import Student from './components/student';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 
 
 function App() {
 
 const [allStudents, setAllStudents] = useState(null);
+const[searchResults, setSearchResults] = useState(null);
+const[keyWords, setKeyWords] = useState("");
+const[gradYear, setGradYear] = useState("");
+
+
+useEffect(() => {
+  saveStudents(students);
+}, []);
+
+const saveStudents = (students) => {
+  setAllStudents(students);
+  setSearchResults(students);
+}
+
+const addStudent = (newStudent) => {
+  const updatedStudents = [...allStudents, newStudent];
+  saveStudents(updatedStudents);
+ }
+
+
+const searchStudents = () => {
+  let keyWordsArray = [];
+
+   if(keyWords) {
+    keyWordsArray = keyWords.toLowerCase(). split(' ');
+  }
+  
+  if(gradYear) {
+    keyWordsArray.push(gradYear.toString());
+  }
+
+
+
+  if(keyWordsArray.length > 0) {
+    const searchResults = allStudents.filter(student => {
+        for(const word of keyWordsArray) {
+          if(student.firstName.toLowerCase().includes(word) || 
+          student.lastName.toLowerCase().includes(word) || 
+          student.gradYear === parseInt(word)) {
+            return true;
+          }
+        }
+        return false;
+      });
+      setSearchResults(searchResults);
+  }
+  else {
+    setSearchResults(allStudents);
+  }
+
+
+
+}
+
+  const removeStudent = (studentToDelete) => {
+    console.table(studentToDelete);
+    const updatedStudentsArray = allStudents.filter(student => student.id !== studentToDelete.id);
+    saveStudents(updatedStudentsArray);
+  }
+
+  const updateStudent = (updatedStudent) => {
+    //console.table(updatedStudent);
+    const updatedStudentsArray = allStudents.map(student => student.id === updatedStudent.id ? {...student, ...updateStudent} : student);
+    saveStudents(updatedStudentsArray);
+  }
 
 
   const students = [{
-  
-
     id: nanoid(),
     firstName: "Dessie",
     lastName:  "Klein",
     email: "dklein@gmail.com",
-    image: 'images/student3.jpg'
+    image: 'images/student3.jpg',
+    gradYear: 2004
   }, {
     id: nanoid(),
     firstName:"Amargo",
       lastName: "Yeo",
      email: "YheAmargo@gmail.com",
-     image: 'images/student10.jpg'
+     image: 'images/student10.jpg',
+     gradYear: 2005
   }, {
     id: nanoid(),
     firstName: "Karina",
     lastName:  "Senussi",
     email:  "ksenussi@gmail.com.",
-    image: 'images/student4.jpg'
+    image: 'images/student4.jpg',
+    gradYear: 2004
   }, {
     id: nanoid(),
     firstName: "Shaun",
     lastName:  "Reed",
-   email: "ShawnR4@gmail.com",
-   image: 'images/student5.jpg'
+   email: "ShaunR4@gmail.com",
+   image: 'images/student5.jpg',
+   gradYear: 2006
   }, {
   
     id: nanoid(),
     firstName:"Rebecca",
     lastName:  "Yancey",
     email: "yancey_Rebecca3@gmail.com",
-    image: 'images/student7.jpg'
+    image: 'images/student7.jpg',
+    gradYear: 2004
   }, {
 
     id: nanoid(),
     firstName:"Eric",
     lastName:  "Calibathe",
     email: "ecal407@yahoo.com",
-    image: 'images/student9.jpg'
+    image: 'images/student9.jpg',
+    gradYear: 2005
   }, {
     id: nanoid(),
     firstName: "Daisy",
     lastName: "Collison",
     email: "dcollison@gmail.com",
-    image: 'images/student1.jpg'
+    image: 'images/student16.jpg',
+    gradYear: 2007
   }, {
     id: nanoid(),
     firstName: "Josiah",
     lastName:  "Kelby",
     email: "Jkelby1@yahoo.com",
-    image: 'images/student2.jpg'
+    image: 'images/student15.jpg',
+    gradYear: 2005
   }, {
     id: nanoid(),
     firstName: "Ami",
      lastName: "Raheed",
     email: "aRaheed@yahoo.com",
-    image: 'images/student6.jpg'
+    image: 'images/student14.jpg',
+    gradYear: 2004
 
   }, {
     id: nanoid(),
     firstName:"Tobiah",
      lastName: "George",
    email: "tge0rge7@gmail.com",
-   image: 'images/student8.jpg'
+   image: 'images/student13.jpg',
+   gradYear: 2007
+
   }, {
-  
+   id: nanoid(),
+   firstName: "Josiah",
+   lastName:  "Kelby",
+   email: "Jkelby1@yahoo.com",
+   image: 'images/student12.jpg',
+   gradYear: 2005
+
+ }, {
+   id: nanoid(),
+   firstName: "Amia",
+    lastName: "Daiee",
+   email: "AMIAdaiee@yahoo.com",
+   image: 'images/student11.jpg',
+   gradYear: 2002
+
+
+ 
   }];
 
 
-  const addStudent = (newStudent) => {
-const updatedStudents = [...allStudents, newStudent];
-setAllStudents(updatedStudents);
-  }
+
 
 
 return (
 <div className= "container">
 
-  <div className="row">
-    {allStudents && allStudents.map((student) =>
+  <div className="row g-4" id="allStudents">
+    <h1 className="collegeName shadow shadow-lg p-3">Elk Valley Community College</h1>
+    <h3 className="currentStudentHeading">Current Students</h3>
+    {searchResults && searchResults.map((student) =>
 ( 
-<div className="col-md-2" key={student.id}>
-  <div className="card h-100">
-    <img class="img-fluid mx-auto" src={student.image} alt="student picture"/>
-    <ul className="list-group list-group-flush">
-      <li className="list-group-item text-center">{student.firstName}</li>
-      <li className="list-group-item text-center">{student.lastName}</li>
-      <li className="list-group-item text-center">{student.email}</li>
-    </ul>
-    </div>
+<div className="col-sm-12 col-md-4 col-lg-3 mb-4" key={student.id}>
+  <Student student={student} removeStudent={removeStudent} updateStudent={updateStudent} />
   </div>)
 )}
+
+
 </div>
-  
  
-  {!allStudents && <button type="button" className="btn btn-lg btn-success" onClick={() => setAllStudents(students)}>Save Students</button>}
+  {/*!allStudents && <button type="button" className="btn btn-lg btn-success" onClick={() => saveStudents(students)}>Save Students</button> */}
   <AddStudent  addStudent={addStudent}/>
+  <div className="row mt-5" id="searchStudents">
+    <h3>Student Search </h3>
+    <div className="col-md-4">
+      <label htmlFor="textKeywords">Search by First Name or Last Name</label>
+      <input type="text" className="form-control" placeholder='Cami Carstens' onChange={evt => setKeyWords(evt.currentTarget.value)} value={keyWords} />
+    </div>
+    <div className="col-md-4 pt-4">
+      <select value={gradYear} onChange={evt => setGradYear(evt.currentTarget.value)} className='form-select'>
+        <option value="">Select Year</option>
+        {_(allStudents).map(student => student.gradYear).sort().uniq().map(year => <option key={year} value={year}>{year}</option>).value()}
+
+      </select>
+    </div>
+
+    <div className="col-md-4 pt-4">
+      <button type="button" className="btn btn-primary" onClick={searchStudents}>Search <FontAwesomeIcon icon={faSearch} /></button>
+    </div>
+  </div>
   </div>
   );
 
 }
 
-export default App
+export default App;
--- FP-CRUD-app/src/components/AddStudent.jsx
+++ FP-CRUD-app/src/components/AddStudent.jsx
@@ -1,15 +1,18 @@
 import React, {useState} from 'react';
 import { nanoid } from 'nanoid';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import './AddStudent.css';
 
 function AddStudent(props) {
 const [firstName,setFirstName] = useState("");
 const [lastName,setLastName] = useState("");
 const[email, setEmail] = useState("");
 const[selectedFile, setSelectedFile] = useState();
+const[gradYear, setGradYear] = useState();
 
 const doWork = () => {
-    const newStudent = {"id":nanoid(), "firstName": firstName, "lastName": lastName, "email": email, "image": URL.createObjectURL(selectedFile) };
+    const newStudent = {"id":nanoid(), "firstName": firstName, "lastName": lastName, "email": email, "image": URL.createObjectURL(selectedFile), "gradYear":parseInt(gradYear)};
     props.AddStudent(newStudent);
 }
 
@@ -18,7 +21,8 @@ const imageUpdate = (event) => {
 }
 
 return (
-    <div className='row mt-'>
+    <div className='row mt-5' id="addStudent">
+        <h3>Add Student</h3>
         <div className='col-md-2'>
             <label htmlFor='txtFirstName' className='form-label'>First Name:</label>
             <input type="text" id="txtFirstName" placeholder="First Name" className="form-control" onChange={(evt) => setFirstName(evt.currentTarget.value)} value={firstName} />
@@ -33,13 +37,24 @@ return (
         <input type="email" id="txtEmail" placeholder="Email Address" className="form-control" onChange={(evt) => setEmail(evt.currentTarget.value)} value={email} />
 
         </div>
+
+        <div className="col-md-2">
+        <label htmlFor='txtGradYear' className='form-label'>Graduation Year:</label>
+        <input type="text" id="txtGradYear" placeholder="2000" className="form-control" onChange={(evt) => setGradYear(evt.currentTarget.value)} value={gradYear} />
+        </div>
+
+
+        
         <div className='col-md-2'>
         <label htmlFor='fileUpload' className='form-label'>Student Image</label>
         <input type='file' name="file" id="fileUpload" onChange={imageUpdate} />
 
         </div>
-        <div className='col-md-4'>
-            <button type="button" id="btnAdd" className="btn btn-lg btn-success" onClick={doWork}>Add Student</button>
+      
+
+
+        <div className='col-md-2'>
+        <button type="button" id="btnAdd" className="btn btn-sm btn-success mt-3 m-5" onClick={doWork}>Add Student <FontAwesomeIcon icon={faPlusCircle} /></button>
 
 
         </div>
